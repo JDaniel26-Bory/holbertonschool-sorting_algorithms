323ecdaa4f080efc516dd3856b21cc227b175528
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-05-24 11:14:46 -0500

    fix the betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,28 @@
 #include "sort.h"
-
+/**
+ * bubble_sort -  the principal function
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i, j;
-    int aux;
-
-    if ((array == NULL) || (size == 0))
-        return;
-
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size -1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                aux = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = aux;
-
-                print_array(array, size);
-
-            }
-        }
-    }
-    return;
+unsigned int i, j;
+int aux;
+if ((array == NULL) || (size == 0))
+return;
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+aux = array[j + 1];
+array[j + 1] = array[j];
+array[j] = aux;
+print_array(array, size);
+}
+}
+}
+return;
 }
